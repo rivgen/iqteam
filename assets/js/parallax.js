584f906c031b62d5be1de8fe5859afa8e967f564
Anatoly <rivgen@gmail.com>
2020-03-06 12:11:35 +0300

    parallax end

---
+++ assets/js/parallax.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    let left = {x: 0, y: 55},
+        right = {x: 53, y: 68}
+    $('body').mousemove(function (e) {
+        parallax(e, document.getElementById('leftId'), 0.2, left);
+        parallax2(e, document.getElementById('rightId'), 0.3, right);
+    });
+//                                        $('.icon-col').click(function () {
+//                                            $('.icon').addClass('flip');
+//                                            console.log(111)
+//                                        });
+});
+
+function parallax(e, target, layer, left) {
+    var strength = 20;
+    var layer_coeff = strength / layer;
+    var x = (left.x - $(window).width() / layer_coeff / 2) + e.pageX / layer_coeff;
+    var y = (left.y - $(window).height() / layer_coeff / 2) + e.pageY / layer_coeff;
+    $(target).css('transform', 'translate(' + x + '%, -' + y + '%)');
+};
+
+function parallax2(e, target, layer, right) {
+    var strength = 20;
+    var layer_coeff = strength / layer;
+    var x = (right.x + $(window).width() / layer_coeff / 2) - e.pageX / layer_coeff;
+    var y = (right.y + $(window).height() / layer_coeff / 2) - e.pageY / layer_coeff;
+    $(target).css('transform', 'translate(' + x + '%, -' + y + '%)');
+};
--- webpack.config.js
+++ webpack.config.js
@@ -26,6 +26,7 @@ Encore
      */
     .addEntry('app', './assets/js/app.js')
     .addEntry('forAdmin', './assets/js/forAdmin.js')
+    .addEntry('parallax', './assets/js/parallax.js')
     //.addEntry('page2', './assets/js/page2.js')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
