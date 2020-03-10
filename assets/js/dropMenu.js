991484f168417f37b17b810374efec333ed6ae76
Fagot <rivgen@gmail.com>
2020-03-10 23:26:47 +0300

    кнопка подняться вверх

--- assets/js/dropMenu.js
+++ assets/js/dropMenu.js
@@ -50,7 +50,8 @@ $(window).scroll(function(){
         $('#home .hadMenu').addClass('fixedCenter')
         $('#home .homeLocale').addClass('fixedLocale')
         $('#home .homeButton').show()
-
+        $('.down object').removeClass('downImg')
+        $('.down object').addClass('upImg')
     }
 })
 $(window).scroll(function(){
@@ -61,9 +62,38 @@ $(window).scroll(function(){
         $('#home .hadMenu').removeClass('fixedCenter')
         $('#home .homeLocale').removeClass('fixedLocale')
         $('#home .homeButton').hide()
+        $('.down object').removeClass('upImg')
+        $('.down object').addClass('downImg')
+
 
     }
 })
+let timerY
+$('.down').mouseover(function () {
+    let yTr = 35
+    transformer(yTr)
+     timerY = setInterval(function () {
+         transformer(yTr)
+            let upDown =  yTr
+         yTr = upDown
+            if (yTr == -80) {
+                yTr = 35
+            } else {
+                yTr = -80
+            }
+
+    }, 1000);
+})
+
+$('.down').mouseout(function () {
+    clearInterval(timerY)
+    transformer(-50)
+});
+
+function transformer(yTr) {
+    $('.downImg').css('transform', 'translate(-50%, ' + yTr + '%)');
+}
+
 //failSend
 var inputs = document.querySelectorAll('.inputfile');
 Array.prototype.forEach.call(inputs, function(input){
--- assets/js/parallax.js
+++ assets/js/parallax.js
@@ -6,83 +6,141 @@ $(document).ready(function () {
         parallax(e, document.getElementById('leftId'), 0.2, left);
         parallax2(e, document.getElementById('rightId'), 0.1, right);
     });
-});
 
 
-function parallax(e, target, layer, left) {
-    var strength = 20;
-    var layer_coeff = strength / layer;
-    let pageY = e.pageY > 1000 ? 1000 : e.pageY
-    var x = (left.x - $(window).width() / layer_coeff / 2) + e.pageX / layer_coeff;
-    var y = (left.y - $(window).height() / layer_coeff / 2) + pageY / layer_coeff;
-    printNumbers(target, x, y);
-    // $(target).css('transform', 'translate3d(' + x + '%, -' + y + '%, 0)');
-};
-
-currentX = 0;
-currentY = 55;
-function printNumbers(target, x, y) {
-
-    xx = x
-    yy = y
-    // console.log('xx1', xx, 'x1', x)
-
-    let timerX = setInterval(function () {
-        $(target).css('transform', 'translate3d(' + currentX + '%, -' + currentY + '%, 0)');
-        // console.log(currentX)
-        if (currentX > xx) {
-            if (currentX != xx) {
-                currentX -= 0.01
-            }
-            timerY()
-            if (currentX < xx) {
-                currentX = xx
-            }
-            if (currentY == yy && currentX == xx) {
-                clearInterval(timerX)
-            }
-        } else if (currentX < xx) {
-            if (currentX != xx) {
-                currentX += 0.01
-            }
-            timerY()
-            if (currentX > xx) {
-                currentX = xx
-            }
-            if (currentY == yy && currentX == xx) {
-                clearInterval(timerX)
-            }
-
-        } else {
-            clearInterval(timerX);
-        }
-    }, 50);
-
-    let timerY = function () {
-        if (currentY > yy) {
-            currentY -= 0.01
-            if (currentY < yy) {
-                currentY = yy
-            }
-
-        } else if (currentY < yy) {
-            currentY += 0.01
-            if (currentY > yy) {
-                currentY = yy
-            }
-        } else {
-            currentY = yy
-        }
+    function parallax(e, target, layer, left) {
+        var strength = 20;
+        var layer_coeff = strength / layer;
+        let pageY = e.pageY > 1000 ? 1000 : e.pageY
+        var x = (left.x - $(window).width() / layer_coeff / 2) + e.pageX / layer_coeff;
+        var y = (left.y - $(window).height() / layer_coeff / 2) + pageY / layer_coeff;
+        printNumbers(target, x, y);
+        // $(target).css('transform', 'translate3d(' + x + '%, -' + y + '%, 0)');
     };
-}
+
+    currentX = left.x;
+    currentY = left.y;
+    function printNumbers(target, x, y) {
+
+        xx = x
+        yy = y
+        // console.log('xx1', xx, 'x1', x)
+
+        let timerX = setInterval(function () {
+            $(target).css('transform', 'translate3d(' + currentX + '%, -' + currentY + '%, 0)');
+            // console.log(currentX)
+            if (currentX > xx) {
+                if (currentX != xx) {
+                    currentX -= 0.01
+                }
+                timerY()
+                if (currentX < xx) {
+                    currentX = xx
+                }
+                if (currentY == yy && currentX == xx) {
+                    clearInterval(timerX)
+                }
+            } else if (currentX < xx) {
+                if (currentX != xx) {
+                    currentX += 0.01
+                }
+                timerY()
+                if (currentX > xx) {
+                    currentX = xx
+                }
+                if (currentY == yy && currentX == xx) {
+                    clearInterval(timerX)
+                }
+
+            } else {
+                clearInterval(timerX);
+            }
+        }, 50);
+
+        let timerY = function () {
+            if (currentY > yy) {
+                currentY -= 0.01
+                if (currentY < yy) {
+                    currentY = yy
+                }
+
+            } else if (currentY < yy) {
+                currentY += 0.01
+                if (currentY > yy) {
+                    currentY = yy
+                }
+            } else {
+                currentY = yy
+            }
+        };
+    }
 
 // использование:
 
-function parallax2(e, target, layer, right) {
-    var strength = 20;
-    var layer_coeff = strength / layer;
-    let pageY = e.pageY > 1000 ? 1000 : e.pageY
-    var x = (right.x + $(window).width() / layer_coeff / 2) - e.pageX / layer_coeff;
-    var y = (right.y + $(window).height() / layer_coeff / 2) - pageY / layer_coeff;
-    $(target).css('transform', 'translate3d(' + x + '%, -' + y + '%, 0)');
-};
+    function parallax2(e, target, layer, right) {
+        var strength = 20;
+        var layer_coeff = strength / layer;
+        let pageY = e.pageY > 1000 ? 1000 : e.pageY
+        var x = (right.x + $(window).width() / layer_coeff / 2) - e.pageX / layer_coeff;
+        var y = (right.y + $(window).height() / layer_coeff / 2) - pageY / layer_coeff;
+        // $(target).css('transform', 'translate3d(' + x + '%, -' + y + '%, 0)');
+        printNumbers2(target, x, y)
+    };
+
+    currentX2 = right.x;
+    currentY2 = right.y;
+    function printNumbers2(target, x, y) {
+
+        xx2 = x
+        yy2 = y
+        // console.log('xx1', xx, 'x1', x)
+
+        let timerX = setInterval(function () {
+            $(target).css('transform', 'translate3d(' + currentX2 + '%, -' + currentY2 + '%, 0)');
+            // console.log(currentX)
+            if (currentX2 > xx2) {
+                if (currentX2 != xx2) {
+                    currentX2 -= 0.01
+                }
+                timerY2()
+                if (currentX2 < xx2) {
+                    currentX2 = xx2
+                }
+                if (currentY2 == yy2 && currentX2 == xx2) {
+                    clearInterval(timerX)
+                }
+            } else if (currentX2 < xx2) {
+                if (currentX2 != xx2) {
+                    currentX2 += 0.01
+                }
+                timerY2()
+                if (currentX2 > xx2) {
+                    currentX2 = xx2
+                }
+                if (currentY2 == yy2 && currentX2 == xx2) {
+                    clearInterval(timerX)
+                }
+
+            } else {
+                clearInterval(timerX);
+            }
+        }, 50);
+
+        let timerY2 = function () {
+            if (currentY2 > yy2) {
+                currentY2 -= 0.01
+                if (currentY2 < yy2) {
+                    currentY2 = yy2
+                }
+
+            } else if (currentY2 < yy2) {
+                currentY2+= 0.01
+                if (currentY2 > yy2) {
+                    currentY2 = yy2
+                }
+            } else {
+                currentY2 = yy2
+            }
+        };
+    }
+});
